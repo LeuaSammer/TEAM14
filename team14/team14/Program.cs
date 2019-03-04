21c5ef0addb95967130a0725fda16de13b7a0454
Leua <leuasammer@gmail.com>
2019-03-04 17:43:20 +0300

    Объявление полей и методов структуры "Работник"

--- team14/team14/Program.cs
+++ team14/team14/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace team14
 {
@@ -56,6 +57,79 @@ namespace team14
         public void InputSalaryMinValue() { }
         public void InputSalaryMaxValue() { }
         #endregion
+    }
+
+    struct Worker
+    {
+        #region Свойства
+
+        /// <summary>
+        /// Фамилия работника
+        /// </summary>
+        public string Surname { get; set; }
+        /// <summary>
+        /// Должность
+        /// </summary>
+        public string Position { get; set; }
+        /// <summary>
+        /// Дата подписания контракта
+        /// </summary>
+        public DateTime ContractSigningDate { get; set; }
+        /// <summary>
+        /// Срок действия контракта
+        /// </summary>
+        public uint ContractTerm { get; set; }
+        /// <summary>
+        /// Оклад
+        /// </summary>
+        public uint Salary { get; set; }
+
+        #endregion
+
+
+        /// <summary>
+        /// Ввод информации о работнике с клавиатуры
+        /// </summary>
+        /// <param name="list">Список работников</param>
+        public static void AddNewWorker(IList<Worker> list)
+        {
+        }
+
+        /// <summary>
+        /// Вывод информации о работнике на экран
+        /// </summary>
+        public void PrintWorker()
+        {
+
+        }
+
+        /// <summary>
+        /// Вывод всех работников на экран
+        /// </summary>
+        /// <param name="list">Список работников</param>
+        public static void PrintAllWorkers(IList<Worker> list)
+        {
+
+        }
+
+        /// <summary>
+        /// Вывод работников, удовлетворяющих фильтру
+        /// </summary>
+        /// <param name="list">Список работников</param>
+        /// <param name="filter">Фильтр</param>
+        public static void PrintFilteredWokers(IList<Worker> list, Filter filter)
+        {
+
+        }
+
+        /// <summary>
+        /// Проверка, удовлетворяет ли работник условиям фильтра
+        /// </summary>
+        /// <param name="filter"></param>
+        public void CheckWithFilter(Filter filter)
+        {
+
+        }
     }
 
     class Program
